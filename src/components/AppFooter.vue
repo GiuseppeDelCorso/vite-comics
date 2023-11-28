8f93d23ef60ec6a69afbd09df493aab57ae5d111
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-28 17:43:38 +0100

    fine

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -18,8 +18,9 @@ export default{
         <div class="container">
             <div class="row">
                 <div class="col-6">
-                    <div>
-                        <ul class="text-white StyleList pt-4">
+                    <div class="row">
+                        <div class="col-3">
+                         <ul class="text-white StyleList pt-4">
                             <li class="fw-bold">DC COMICS</li>
                                 <li class="fs-8 py-1 TextColor">
                                     Charatter
@@ -51,14 +52,126 @@ export default{
                                 <li class="fs-8 py-1 TextColor">
                                     NEWS
                                 </li>
-                                <li class="fs-8 py-1 TextColor">SHOP
-
+                                <li class="fw-bold">
+                                    SHOP
                                 </li>
-                        </ul>
+                                <li class="fs-8 py-1 TextColor">
+                                    Shop DC
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    Shop DC Collection
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-3">
+                         <ul class="text-white StyleList pt-4">
+                            <li class="fw-bold">DC COMICS</li>
+                                <li class="fs-8 py-1 TextColor">
+                                    Charatter
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    COMICS
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    MOVIES
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    TV
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    GAMES
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    GAMES
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    COLLECTIBLES
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    VIDEOS
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    FANS
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    NEWS
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    JOBS
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    Shop DC
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    Shop DC Collection
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-3">
+                         <ul class="text-white StyleList pt-4">
+                            <li class="fw-bold">DC COMICS</li>
+                                <li class="fs-8 py-1 TextColor">
+                                    Charatter
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    COMICS
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    MOVIES
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    TV
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    GAMES
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    GAMES
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    COLLECTIBLES
+                                </li>
+                                <li class="fs-8 py-1 TextColor">
+                                    VIDEOS
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
                 <div class="col-6">
                     <img class="sizeImg" src="/assets/dc-logo-bg.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="BgFooter">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <button class="BottonStyle my-3">SIGN-UO-NOW!</button>
+                </div>
+                <div class="col-6">
+                    <div class="row d-flex justify-content-end">
+                        <div class="col-3">
+                            <div class="textStyle mt-4">FOLLOW US</div>
+                        </div>
+                        <div class="col-1 mt-4">
+                            <img src="/assets/footer-facebook.png" alt="">
+                        </div>
+                        <div class="col-1 mt-4">
+                            <img src="/assets/footer-twitter.png" alt="">
+                        </div>
+                        <div class="col-1 mt-4">
+                            <img src="/assets/footer-youtube.png" alt="">
+                        </div>
+                        <div class="col-1 mt-4">
+                            <img src="/assets/footer-pinterest.png" alt="">
+                        </div>
+                        <div class="col-1 mt-4">
+                            <img src="/assets/footer-perioscope.png" alt="">
+                        </div>s
+                    </div>
+
                 </div>
             </div>
         </div>
@@ -81,8 +194,24 @@ export default{
         font-size: 10px;
     }
     .TextColor{
-        color: #5b5d5c;
+        color: #8e948d;
     }
-
+    .BgFooter{
+        background-color: #303030;
+    }
+    .BottonStyle{
+        border-color: #0382f8;
+        background-color: #303030;
+        color: white;
+        width: 180px;
+        height: 50px;
+        font-size: 15px;
+        font-weight: bold;
+    }
+    .textStyle{
+        color: #0382f8;
+        font-weight: bold;
+        font-size: 20px;
+    }
 
 </style>
